e60ade5b6f10e5af6c37de756ad1f0d5d6f54de4
Ajith Kanumuri <21294096+ajith05@users.noreply.github.com>
2024-08-26 16:45:09 +0000

    Add python script with error

---
+++ myfile.py
@@ -0,0 +1,10 @@
+def odd_even(x):
+    return x % 2 == 0
+
+def show_result(x):
+    ans = 'even' if odd_even(x) else 'odd
+    print(f'{x} is {ans}')
+
+show_result(5)
+show_result(11)
+show_result(16)
