b61c72cb3fa94f1601d284680eb9a656e67b8b4d
suhasadidela <101173171+suhasadidela@users.noreply.github.com>
2024-08-27 17:50:39 +0000

    we added the file

--- myfile.py
+++ myfile.py
@@ -2,7 +2,7 @@ def odd_even(x):
     return x % 2 == 0
 
 def show_result(x):
-    ans = 'even' if odd_even(x) else 'odd
+    ans = 'even' if odd_even(x) else 'odd'
     print(f'{x} is {ans}')
 
 show_result(5)
