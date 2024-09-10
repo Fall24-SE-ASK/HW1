0d0fe0e95e259b9eeae69657169e45f2d20d56ce
Ajith Kanumuri <21294096+ajith05@users.noreply.github.com>
2024-09-10 18:38:33 +0000

    Made pylint suggested changes to rand.py

--- HW2/rand.py
+++ HW2/rand.py
@@ -1,10 +1,19 @@
+'''
+Helper functions for `hw2_debugging.py`
+'''
 import subprocess
 
 
 def random_array(arr):
+    '''
+    Generates an array of random numbers
+    '''
     shuffled_num = None
-    for i in range(len(arr)):
+    for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
+            ["shuf", "-i1-20", "-n1"],
+            capture_output=True,
+            check=False
+        )
         arr[i] = int(shuffled_num.stdout)
     return arr
